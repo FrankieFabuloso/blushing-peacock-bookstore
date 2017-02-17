d7e0758eb9c683bec7b1fc0fd403c1ee560a588b
FrankieFabuloso <frocha@ucsc.edu>
2017-02-16 17:48:03 -0800

    WIP- adds a book view which allows the inline editing of title and description

--- database/db.js
+++ database/db.js
@@ -9,15 +9,24 @@ const db = pgp(conectionString)
 
 
 const GET_ALL = "SELECT book.*, author.author_name, genre.book_genre FROM book JOIN author ON author_id=author.id JOIN genre ON genre_id=genre.id ORDER BY book.id ASC"
+const GET_ONE = "SELECT book.*, author.author_name, genre.book_genre FROM book JOIN author ON author_id=author.id JOIN genre ON genre_id=genre.id WHERE book.id = $1"
 const DELETE_BOOK = "DELETE FROM book WHERE id = $1"
 const ADD_BOOK = "INSERT INTO book (title, description) VALUES ('$1', '$2') returning *;"
-const UPDATE_BOOK = "UPDATE book SET title='$1' WHERE id=$2;"
+const UPDATE_BOOK_TITLE = `UPDATE book SET title=$3 WHERE id=$1;`
+const UPDATE_BOOK_DESCRIPTION = `UPDATE book SET description=$3 WHERE id=$1;`
 
 const Books = {
   getAll: () => db.any(GET_ALL),
+  getById: (id) => db.one(GET_ONE, [id]),
   deleteOne: (id) => db.none(DELETE_BOOK, [id]),
   add: (title, description) => db.one(ADD_BOOK, [title, description]),
-  update: (title, id) => db.none(UPDATE_BOOK, [title, id])
+  update: (id, column, data) => {
+    if( column === 'title') {
+      return db.none(UPDATE_BOOK_TITLE, [id, column, data])
+    } else {
+      return db.none(UPDATE_BOOK_DESCRIPTION, [id, column, data])
+    }
+  }
 }
 
 module.exports = Books
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  res.render('layout', { message: 'Hello there!' })
+  res.render('layout')
 })
 
 router.get('/book/:id', function (req, res) {
-  res.render('layout', { message: 'Hello there!' })
+  res.render('book')
 })
 
 module.exports = router
